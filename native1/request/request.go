c16b54bcbce20ea9d22695d1fda8f3e426d32628
SamuelRise <Samuel.Benichou@risecodes.com>
2025-04-08 12:09:01 +0300

    RPRS-3285: Revert last commit (#7)

--- native1/request/request.go
+++ native1/request/request.go
@@ -161,7 +161,7 @@ type Request struct {
 	//   array of objects
 	// Description:
 	//   Specifies what type of event tracking is supported - see Event Trackers Request Object
-	EventTrackers []EventTracker `json:"eventrackers,omitempty"`
+	EventTrackers []EventTracker `json:"eventtrackers,omitempty"`
 
 	// Field:
 	//   privacy
