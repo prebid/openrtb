0d01af1cd27503d7edd0584fadb7e740ab83cc9c
Postindustria <oss@postindustria.com>
2026-05-14 22:05:28 +0300

    Update IPLocationService type, add 51Degrees support (#25)

--- adcom1/ip_location_service.go
+++ adcom1/ip_location_service.go
@@ -1,12 +1,14 @@
 package adcom1
 
 // IPLocationService represents services and/or vendors used for resolving IP addresses to geolocations.
-type IPLocationService int8
+type IPLocationService int16
 
 // Services and/or vendors used for resolving IP addresses to geolocations.
 const (
-	LocationServiceIP2Location IPLocationService = 1 // ip2location
-	LocationServiceNeustar     IPLocationService = 2 // Neustar (Quova)
-	LocationServiceMaxMind     IPLocationService = 3 // MaxMind
-	LocationServiceNetAcuity   IPLocationService = 4 // NetAcuity (Digital Element)
+	LocationServiceIP2Location               IPLocationService = 1   // ip2location
+	LocationServiceNeustar                   IPLocationService = 2   // Neustar (Quova)
+	LocationServiceMaxMind                   IPLocationService = 3   // MaxMind
+	LocationServiceNetAcuity                 IPLocationService = 4   // NetAcuity (Digital Element)
+	LocationService51DegreesHighConfidence   IPLocationService = 511 // 51Degrees (High Confidence)
+	LocationService51DegreesMediumConfidence IPLocationService = 512 // 51Degrees (Medium Confidence)
 )
